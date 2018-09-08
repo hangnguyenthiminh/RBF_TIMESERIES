74cfa08ba2b52b7e5c09a69921d87f3c01576de4
hangnguyenthiminh <nguyenthiminhhang.mta@gmail.com>
2018-09-08 16:48:19 +0700

    code step 1

--- RBF_TIMESERIES/RBF_TIMESERIES/MainForm.Designer.cs
+++ RBF_TIMESERIES/RBF_TIMESERIES/MainForm.Designer.cs
@@ -30,23 +30,28 @@
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataList = new System.Windows.Forms.ListView();
-            this.realData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.forcasteData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colIndex = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colRealData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTrainData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTestData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colForcasteData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.btnLoadData = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTestingRate = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtWindowSize = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTestErr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTrainErr = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -55,36 +60,55 @@
             this.groupBox1.Controls.Add(this.btnLoadData);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(229, 450);
+            this.groupBox1.Size = new System.Drawing.Size(395, 450);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Data";
+            this.groupBox1.Text = "Load data to train";
             // 
             // dataList
             // 
             this.dataList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.realData,
-            this.forcasteData});
+            this.colIndex,
+            this.colRealData,
+            this.colTrainData,
+            this.colTestData,
+            this.colForcasteData});
+            this.dataList.FullRowSelect = true;
+            this.dataList.GridLines = true;
             this.dataList.Location = new System.Drawing.Point(6, 19);
             this.dataList.Name = "dataList";
-            this.dataList.Size = new System.Drawing.Size(217, 396);
+            this.dataList.Size = new System.Drawing.Size(383, 396);
             this.dataList.TabIndex = 0;
             this.dataList.UseCompatibleStateImageBehavior = false;
             this.dataList.View = System.Windows.Forms.View.Details;
             // 
-            // realData
+            // colIndex
             // 
-            this.realData.Text = "Real Data";
-            this.realData.Width = 67;
+            this.colIndex.Text = "STT";
+            this.colIndex.Width = 34;
             // 
-            // forcasteData
+            // colRealData
             // 
-            this.forcasteData.Text = "Forcaste Data";
-            this.forcasteData.Width = 95;
+            this.colRealData.Text = "Real Data";
+            this.colRealData.Width = 67;
+            // 
+            // colTrainData
+            // 
+            this.colTrainData.Text = "Train Data";
+            this.colTrainData.Width = 74;
+            // 
+            // colTestData
+            // 
+            this.colTestData.Text = "Test Data";
+            // 
+            // colForcasteData
+            // 
+            this.colForcasteData.Text = "Forcaste Data";
+            this.colForcasteData.Width = 95;
             // 
             // btnLoadData
             // 
-            this.btnLoadData.Location = new System.Drawing.Point(74, 421);
+            this.btnLoadData.Location = new System.Drawing.Point(152, 421);
             this.btnLoadData.Name = "btnLoadData";
             this.btnLoadData.Size = new System.Drawing.Size(75, 23);
             this.btnLoadData.TabIndex = 1;
@@ -94,112 +118,127 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(247, 12);
+            this.groupBox2.Location = new System.Drawing.Point(413, 12);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(411, 450);
+            this.groupBox2.Size = new System.Drawing.Size(469, 450);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.label3);
-            this.groupBox3.Controls.Add(this.textBox3);
-            this.groupBox3.Controls.Add(this.label2);
-            this.groupBox3.Controls.Add(this.textBox2);
             this.groupBox3.Controls.Add(this.label4);
-            this.groupBox3.Controls.Add(this.textBox4);
+            this.groupBox3.Controls.Add(this.txtTestingRate);
             this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Controls.Add(this.textBox1);
-            this.groupBox3.Controls.Add(this.btnStart);
-            this.groupBox3.Location = new System.Drawing.Point(664, 12);
+            this.groupBox3.Controls.Add(this.txtWindowSize);
+            this.groupBox3.Location = new System.Drawing.Point(888, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(200, 450);
+            this.groupBox3.Size = new System.Drawing.Size(200, 243);
             this.groupBox3.TabIndex = 0;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Settings";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 146);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Test Error";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(113, 143);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(56, 20);
-            this.textBox3.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 107);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Train Error";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(113, 104);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(56, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 67);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Window Size";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(113, 64);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(56, 20);
-            this.textBox1.TabIndex = 2;
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(63, 421);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            // 
-            // openFileDialog
-            // 
-            this.openFileDialog.Filter = "All Files|*.*|Text File (.txt)|*.txt";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(113, 25);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(56, 20);
-            this.textBox4.TabIndex = 2;
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 28);
+            this.label4.Location = new System.Drawing.Point(27, 38);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(80, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Testing rate (%)";
             // 
+            // txtTestingRate
+            // 
+            this.txtTestingRate.AllowDrop = true;
+            this.txtTestingRate.Location = new System.Drawing.Point(113, 35);
+            this.txtTestingRate.Name = "txtTestingRate";
+            this.txtTestingRate.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtTestingRate.Size = new System.Drawing.Size(56, 20);
+            this.txtTestingRate.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Window Size";
+            // 
+            // txtWindowSize
+            // 
+            this.txtWindowSize.Location = new System.Drawing.Point(113, 74);
+            this.txtWindowSize.Name = "txtWindowSize";
+            this.txtWindowSize.Size = new System.Drawing.Size(56, 20);
+            this.txtWindowSize.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Test Error";
+            // 
+            // txtTestErr
+            // 
+            this.txtTestErr.Location = new System.Drawing.Point(113, 68);
+            this.txtTestErr.Name = "txtTestErr";
+            this.txtTestErr.ReadOnly = true;
+            this.txtTestErr.Size = new System.Drawing.Size(56, 20);
+            this.txtTestErr.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Train Error";
+            // 
+            // txtTrainErr
+            // 
+            this.txtTrainErr.Location = new System.Drawing.Point(113, 29);
+            this.txtTrainErr.Name = "txtTrainErr";
+            this.txtTrainErr.ReadOnly = true;
+            this.txtTrainErr.Size = new System.Drawing.Size(56, 20);
+            this.txtTrainErr.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(67, 172);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Text File (.txt)|*.txt";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnStart);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.txtTestErr);
+            this.groupBox4.Controls.Add(this.txtTrainErr);
+            this.groupBox4.Location = new System.Drawing.Point(888, 261);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 201);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Result";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(876, 509);
+            this.ClientSize = new System.Drawing.Size(1100, 526);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -210,6 +249,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -218,21 +259,25 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ListView dataList;
-        private System.Windows.Forms.ColumnHeader realData;
-        private System.Windows.Forms.ColumnHeader forcasteData;
+        private System.Windows.Forms.ColumnHeader colRealData;
+        private System.Windows.Forms.ColumnHeader colForcasteData;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button btnLoadData;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtTestErr;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTrainErr;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtWindowSize;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtTestingRate;
+        private System.Windows.Forms.ColumnHeader colIndex;
+        private System.Windows.Forms.ColumnHeader colTrainData;
+        private System.Windows.Forms.ColumnHeader colTestData;
+        private System.Windows.Forms.GroupBox groupBox4;
     }
 }
 
--- RBF_TIMESERIES/RBF_TIMESERIES/MainForm.cs
+++ RBF_TIMESERIES/RBF_TIMESERIES/MainForm.cs
@@ -12,8 +12,19 @@ namespace RBF_TIMESERIES
     public partial class MainForm : Form
     {
         private double[] data = null;
+        private double[][] allData = null;
         private int windowSize = 5;
-        private double[,] dataToShow = null;
+        private double testingRate = 30;
+
+        //Train
+        private double[] train = null;
+        private double[][] inputTrain = null;
+        private double[][] idealTrain = null;
+
+        //Test
+        private double[] test = null;
+        private double[][] inputTest = null;
+        private double[][] idealTest = null;
 
 
         public MainForm()
@@ -30,7 +41,77 @@ namespace RBF_TIMESERIES
             // add new records
             for (int i = 0, n = data.GetLength(0); i < n; i++)
             {
-                dataList.Items.Add(data[i].ToString());
+                //add STT column
+                dataList.Items.Add((i + 1).ToString());
+                //add Real Data column (thêm lần lượt value vào các cột sau...thêm trên một hàng)
+                //ListView1.Items[index của tên cột đầu tiên].SubItems.Add("thêm từng cột")
+                dataList.Items[i].SubItems.Add(data[i].ToString());
+            }
+        }
+
+        private double[] GetWindowData(int index, int windowSize, double[] data)
+        {
+            double[] subData = new double[windowSize];
+            for (int i = 0; i < windowSize; i++)
+            {
+                subData[i] = data[index + i];
+            }
+            return subData;
+        }
+
+
+        /// <summary>
+        /// Get data to input[][] and ideal[][]
+        /// </summary>
+        /// <param name="data">data train or test</param>
+        /// <param name="input"></param>
+        /// <param name="ideal"></param>
+        /// <param name="windowSize"></param>
+        private void GetInputIdeal(double[] data, out double[][] input, out double[][] ideal, int windowSize)
+        {
+            int sizeData = data.Length;
+            //Get input, ideal cho train
+            input = new double[sizeData - windowSize][];
+            ideal = new double[sizeData - windowSize][];
+
+            for (int i = 0; i < sizeData - windowSize; i++)
+            {
+                input[i] = GetWindowData(i, windowSize, data);
+                ideal[i] = new double[] { data[i + windowSize] };
+            }
+        }
+
+        /// <summary>
+        /// Get data to train[] and test[]
+        /// </summary>
+        /// <param name="data">all data get from File</param>
+        /// <param name="train"></param>
+        /// <param name="test"></param>
+        /// <param name="testingRate"></param>
+        private void GetTrainTest(double[] data, out double[] train, out double[] test, double testingRate)
+        {
+            //khoi tao
+            int sizeData = data.Length;
+            int numTest = (int)((testingRate / 100) * sizeData);
+            int numTrain = sizeData - numTest;
+            train = new double[numTrain];
+            test = new double[numTest];
+            int j = 0;
+            int k = 0;
+
+            //Get data train and test from data[]
+            for (int i = 0; i < sizeData; i++)
+            {
+                if (i < numTrain)
+                {
+                    train[j] = data[i];
+                    j++;
+                }
+                else
+                {
+                    test[k] = data[i];
+                    k++;
+                }
             }
         }
 
@@ -40,34 +121,36 @@ namespace RBF_TIMESERIES
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 StreamReader reader = null;
-                // read maximum 50 points
-                double[] tempData = new double[50];
+                // read maximum 1000 points
+                double[] tempData = new double[1000];
 
                 try
                 {
                     // open selected file
                     reader = File.OpenText(openFileDialog.FileName);
-                    string str = null;
                     int i = 0;
 
                     // read the data
-                    while ((i < 50) && ((str = reader.ReadLine()) != null))
-                    {
-                        // parse the value
-                        tempData[i] = double.Parse(str);
 
+                    string[] lines = System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+                    // Display the file contents by using a foreach loop.
+                    System.Console.WriteLine("Contents of WriteLines2.txt = ");
+                    foreach (string line in lines)
+                    {
+                        // Use a tab to indent each line of the file.
+                        int pos = line.IndexOf("\t");
+                        string value = line.Substring(pos + 1, line.Length - 1 - pos);
+                        //parse the value
+                        tempData[i] = double.Parse(value);
                         i++;
+
                     }
 
                     // allocate and set data
                     data = new double[i];
-                    dataToShow = new double[i, 2];
                     Array.Copy(tempData, 0, data, 0, i);
-                    for (int j = 0; j < i; j++)
-                    {
-                        dataToShow[j, 0] = j;
-                        dataToShow[j, 1] = data[j];
-                    }
+
                 }
                 catch (Exception)
                 {
@@ -86,6 +169,13 @@ namespace RBF_TIMESERIES
                 // enable "Start" button
                 //startButton.Enabled = true;
             }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GetTrainTest(data, out train, out test, testingRate);
+            GetInputIdeal(train, out inputTrain, out idealTrain, windowSize);
+            GetInputIdeal(train, out inputTest, out idealTest, windowSize);
         }
     }
 }
